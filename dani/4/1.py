cfc0d3a186c8159b837b4ab06fa842f1f53b7153
dani10mv <danielmirallesvargas@gmail.com>
2022-04-30 21:12:10 +0200

    dani :  day4

--- dani/4/1.py
+++ dani/4/1.py
@@ -1,7 +1,4 @@
 
-
-mBoard = [[] * 5 for i in range(5)]
-mCall = [[] * 5 for i in range(5)]
 def getNullCallBoard():
     mCall = [[] * 5 for i in range(5)]
     for i in range(5):
@@ -12,14 +9,14 @@ def check(mCall, row, col):
     count = 0
     #check row
     for i in range(5):
-        if mCall[row,i]:
+        if mCall[row][i]:
             count +=1
     if count == 5 :
         return True
     count = 0
     #check col
     for i in range(5):
-        if mCall[i,col]:
+        if mCall[i][col]:
             count +=1
     if count == 5 :
         return True
@@ -27,9 +24,30 @@ def check(mCall, row, col):
     return False
 
 def call(num, lBoard, lCall):
-    for board in lBoard:
+    i = 0
+    while i < len(lBoard):
+        board = lBoard[i]
+        calls = lCall[i]
+
         #search the number
-        
+        for row in range(5):
+            for col in range(5):
+                if board[row][col] == num:
+                    calls[row][col] = True
+                    if check(calls, row, col):
+                        return [board,calls]
+        i += 1
+    return []
+
+def calculateScore(board, calls, lastCall):
+    score= 0
+    for row in range(5):
+        for col in range(5):
+            if not calls[row][col]:
+                score += int(board[row][col])
+    print (score)
+    return score * int(lastCall)
+
 
 
 def main():
@@ -37,25 +55,41 @@ def main():
     file = open("input.txt","r")
 
     lcalled = file.readline().rstrip('\n').split(",")
-    print(lcalled)
     lBoard = []
     lCall = []
  
     line = file.readline()                      #first time is \n
+    
     while line != "":
-        print("holas")
+      
         mBoard = [[] * 5 for i in range(5)]
         for i in range(5):
             row = file.readline().rstrip('\n').split()
             mBoard[i]=row
-        line = file.readline()
         lBoard.append(mBoard)
         lCall.append(getNullCallBoard())
         line = file.readline() 
+        
+
+
     i = 0
     winner = []
-    while winner == [] :
-        winner = call(callNum, lBoard, lCall)
+    found = False
+    
+    while i< len(lcalled) and not found :
+        result = call(lcalled[i], lBoard, lCall)
+        if result != [] :
+            lastCall = lcalled[i]
+            winner = result
+            print(lastCall)
+            print(result)
+            found = True
+        i+= 1
+
+    winnerBoard = winner[0]
+    winnerCalls = winner[1]
+    
+    print(calculateScore(winnerBoard,winnerCalls,lastCall))
     
         
 
