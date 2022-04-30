a4562d9b024bb6c6c4a2d6a7ea1b7cbd4c149be0
dani10mv <danielmirallesvargas@gmail.com>
2022-04-30 18:19:20 +0200

    crear matrix

---
+++ dani/4/1.py
@@ -0,0 +1,25 @@
+
+
+mBoard = [[] * 5 for i in range(5)]
+mCall = [[] * 5 for i in range(5)]
+
+def main():
+
+    file = open("input.txt","r")
+
+    lcalled = file.readline().split(",")
+    print(lcalled)
+    lBoard = []
+    lCall = []
+
+    line = file.readline()                      #first time is \n
+    while line != "":
+        mBoard = [[] * 5 for i in range(5)]
+        line
+        
+
+    
+
+
+if __name__=='__main__':
+  main()
